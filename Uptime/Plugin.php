e5113b023b3a4f6781c43f35d54b2c2ec39f69aa
rainfroid <aprikyblue@gmail.com>
2017-05-29 21:33:30 +0800

    fix #1

--- Uptime/Plugin.php
+++ Uptime/Plugin.php
@@ -4,7 +4,7 @@
  * 
  * @package Uptime
  * @author aprikyblue
- * @version 1.0.0
+ * @version 1.0.1
  * @link https://github.com/aprikyblue/Typecho-Plugin-Uptime/
  */
 class Uptime_Plugin implements Typecho_Plugin_Interface
@@ -42,9 +42,9 @@ class Uptime_Plugin implements Typecho_Plugin_Interface
     {
         /** 开始时间 */
         $start_time = new Typecho_Widget_Helper_Form_Element_Text(
-            'start_time', NULL, '2014-01-01 00:00:00',
-            _t('开始时间'),
-            _t('可以使用Javascript的Date构造参数接受的格式')
+            'start_time', NULL, '2014/01/01 00:00:00',
+            _t('开始时间（博客服务器时区）'),
+            _t('可以使用php的strtotime()接受的格式，详细参见http://php.net/manual/zh/datetime.formats.time.php')
         );
         $form->addInput($start_time);
     }
@@ -85,8 +85,10 @@ class Uptime_Plugin implements Typecho_Plugin_Interface
 <!-- Uptime Start -->
 <script>
 setInterval(function(){
-	var start_time = '<?php echo $settings->start_time; ?>';
-    var times = new Date().getTime() - new Date(start_time).getTime();
+	var start_timestamp = <?php echo strtotime($settings->start_time)*1000;
+	// timestamp is seconds in php but milliseconds in js, difference of 1000 times.
+	?>;
+	var times = new Date().getTime() - new Date(start_timestamp).getTime();
     times = Math.floor(times/1000); // convert total milliseconds into total seconds
     var days = Math.floor( times/(60*60*24) ); //separate days
     times %= 60*60*24; //subtract entire days
